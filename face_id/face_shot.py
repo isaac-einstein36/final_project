8c02bbb4039ecb4d2f1e9cbc6115602f4940541c
Isaac Einstein <einstein.15@osu.edu>
2025-04-28 10:06:37 -0400

    new faceID

--- face_id/face_shot.py
+++ face_id/face_shot.py
@@ -4,7 +4,7 @@ import cv2
 import os
 from picamera2 import Picamera2
 
-name = 'Katie'  # Replace with your name
+name = 'Dr.Metzler'  # Replace with your name
 
 # Create the directory if it doesn't exist
 output_dir = f"dataset/{name}/"
